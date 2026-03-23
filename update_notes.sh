1651c0bbac03180f86952f1ac54898942b810c2a
Tomas Gutierrez <tgut03@gmail.com>
2026-03-23 16:47:04 -0500

    add .md filter

--- update_notes.sh
+++ update_notes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 python3 update_notes.py
-git add .
-git commit -m "update notes"
+git add "*.md"
+git diff --cached --quiet || git commit -m "update notes"
 git push
