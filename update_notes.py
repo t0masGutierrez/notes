e16c976d331236b7617560293cd443baf16d3616
Tomas Gutierrez <tgut03@gmail.com>
2026-03-31 12:47:14 -0500

    remove renamed/deleted files

--- update_notes.py
+++ update_notes.py
@@ -4,32 +4,34 @@ def update_notes():
     input = os.path.expanduser("~") + "/Obsidian/brainTwo/"
     output = os.path.expanduser("~") + "/Github/notes/"
     dirs = {}
+
     # loop through folders inside obsidian
     for dir in os.listdir(input):
         ignore = ["Templates", "Brain.base"]
         if not dir.startswith(".") and dir not in ignore:
             folder_name = dir
-            folder_path = input + folder_name
+            folder_path = os.path.join(input, folder_name)
             dirs[folder_name] = {}
 
             # loop through files inside obsidian folder
             for file in os.listdir(folder_path):
                 if file.endswith(".md"):
-                    file_path = folder_path + "/" + file
+                    file_path = os.path.join(folder_path, file)
                     file_name = os.path.basename(file)[:-3] # remove suffix ".md"
                     dirs[folder_name][file_name] = file_path
 
                     # read from obsidian files
                     with open(file_path, "r") as obs:
                         data = obs.read()
-                        os.makedirs(output + folder_name + "/", exist_ok=True)
+                        os.makedirs(os.path.join(output, folder_name), exist_ok=True)
 
                         # write to github files
-                        with open(output + folder_name + "/" + file_name + ".md", "w") as git:
+                        out_file = os.path.join(output, folder_name, file_name + ".md")
+                        with open(out_file, "w") as git:
                             git.write(data)
 
                         # read from github files
-                        with open(output + folder_name + "/" + file_name + ".md", "r") as git:
+                        with open(out_file, "r") as git:
                             lines = git.readlines()
 
                             # loop through lines inside file
@@ -57,8 +59,19 @@ def update_notes():
                                     lines[i] = lines[i].split("![[")[0]
                             
                             # write to github files
-                            with open(output + folder_name + "/" + file_name + ".md", "w") as git:
+                            with open(out_file, "w") as git:
                                 git.writelines(lines)
+
+    # remove renamed/deleted files
+    for folder_name in os.listdir(output):
+        out_folder = os.path.join(output, folder_name)
+        in_folder = os.path.join(input, folder_name)
+        if os.path.isdir(out_folder) and os.path.isdir(in_folder):
+            obsidian_files = set(f for f in os.listdir(in_folder) if f.endswith('.md'))
+            github_files = set(f for f in os.listdir(out_folder) if f.endswith('.md'))
+            for file in github_files:
+                if file not in obsidian_files:
+                    os.remove(os.path.join(out_folder, file))
 
 def main():
     update_notes()
